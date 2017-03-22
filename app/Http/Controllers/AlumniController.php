a0e7f9fe9839a4b8fb78ec7bce5e0c014269546a
mmlmml1 <huqingyang2000@gmail.com>
2017-03-22 17:13:13 +0800

    Initial Project

---
+++ app/Http/Controllers/AlumniController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+
+class AlumniController extends Controller
+{
+    //
+    function auth($step){
+    	return $step;
+    }
+}
