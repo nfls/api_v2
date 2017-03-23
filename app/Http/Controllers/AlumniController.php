bc7384108427ed1111035504dbbbee1dc28678a4
mmlmml1 <huqingyang2000@gmail.com>
2017-03-23 10:14:22 +0800

    Add basic database operations

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
 
 class AlumniController extends Controller
 {
     //
     function auth($step){
-    	return $step;
-    }
+    	if(is_numeric($step)==true){
+    		$user_id = DB::table("user_auth")->pluck("id")->first();
+    		if(is_null($user_id))
+    			return "Database is empty!";
+    		return "Found the first one:".$user_id;
+    	}
+    	else
+    		abort(403,"Check your input.");
+	}
 }
