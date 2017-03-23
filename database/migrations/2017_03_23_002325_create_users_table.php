c1b92411e877d43bdb282326f063c6fbee6804a9
mmlmml1 <huqingyang2000@gmail.com>
2017-03-23 10:14:43 +0800

    Modify migrations

---
+++ database/migrations/2017_03_23_002325_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('user_auth')) {
+            Schema::create('user_auth', function (Blueprint $table) {
+                $table->unsignedBigInteger('id')->unique();
+                $table->json('auth_info')->nullable();
+                $table->json('primary_school')->nullable();
+                $table->json('junior_school')->nullable();
+                $table->json('senior_school')->nullable();
+                $table->json('college')->nullable();
+                $table->json('working_info')->nullable();
+                $table->json('personal_info')->nullable();
+                $table->index('id');
+            });
+        }
+        
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_auth');
+    }
+}
