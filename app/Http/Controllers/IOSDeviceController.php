9ac36e45be78a74835db83c1cf17f176c4b1a7e3
mmlmml1 <huqingyang2000@gmail.com>
2017-07-02 17:32:00 -0400

    Update database queries

--- app/Http/Controllers/IOSDeviceController.php
+++ app/Http/Controllers/IOSDeviceController.php
@@ -11,9 +11,12 @@ class IOSDeviceController extends Controller
     function registerDevice(Request $request){
         $device_id = $request->input("device_id");
         $device_model = $request->input("device_model");
-        if(strlen($device_id)!=32)
+        if(strlen($device_id)!=64)
             abort(403);
-        DB::connection("mysql_user")->table("user_device")->insert(["device_id"=>$device_id,"device_model"=>$device_model]);
+        $device = DB::connection("mysql_user")->table("user_device")->where(["device_id"=>$device_id])->first();
+        if(is_null(@$device->device_model)){
+            DB::connection("mysql_user")->table("user_device")->insert(["device_id" => $device_id, "device_model" => $device_model, "user_id"=>-1]);
+        }
         return Response::json(array("code"=>200, "status"=>"succeed"));
     }
 }
