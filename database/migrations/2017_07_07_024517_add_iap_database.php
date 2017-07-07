dbcafa37ecb2fc3f21ceeb84c6277392e1b1d149
mmlmml1 <huqingyang2000@gmail.com>
2017-07-06 22:47:19 -0400

    add user purchase db

---
+++ database/migrations/2017_07_07_024517_add_iap_database.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIapDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql_user')->create('user_purchase', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->unique();
+            $table->string("receipt");
+            $table->integer("user_id")->default(-1);
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('mysql_user')->dropIfExists('user_session');
+    }
+}
