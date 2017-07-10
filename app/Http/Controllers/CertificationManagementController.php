6212e7c04c94870479fdf908db6d9167a9ea3ee4
mmlmml1 <huqingyang2000@gmail.com>
2017-07-10 13:08:35 -0400

    Divide it into two parts

--- app/Http/Controllers/CertificationManagementController.php
+++ app/Http/Controllers/CertificationManagementController.php
@@ -10,7 +10,9 @@ use Illuminate\Support\Facades\DB;
 class CertificationManagementController extends Controller
 {
 
-    //
+    function getUserDetail(Request $request){
+
+    }
     function getSubmittedUserList(Request $request){
         if(!UserCenterController::checkAdmin(1))
             abort(403);
@@ -19,7 +21,8 @@ class CertificationManagementController extends Controller
         $info = DB::connection('mysql_alumni')->table('user_auth')->whereNull("status_change_time")->whereNotNull("submit_time")->get();
         $cut = array("cut"=>"");
         foreach($info as $user){
-            array_push($return,(array("id"=>$user->id)  + json_decode($user->auth_info,true) + array("cut2"=>"") + json_decode($user->primary_school,true) + array("cut3"=>"") + json_decode($user->junior_school,true) + array("cut4"=>"") + json_decode($user->senior_school,true)));
+            array_push($return, array("submit_time"=>$user->submit_time,"id"=>$user->id));
+            //array_push($return,(array("id"=>$user->id)  + json_decode($user->auth_info,true) + array("cut2"=>"") + json_decode($user->primary_school,true) + array("cut3"=>"") + json_decode($user->junior_school,true) + array("cut4"=>"") + json_decode($user->senior_school,true)));
         }
         return Response::json($return);
     }
