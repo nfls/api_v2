a22b6a966d303e646525355a2ad6b8737c364c66
mmlmml1 <huqingyang2000@gmail.com>
2017-07-15 10:02:23 -0400

    current_id

--- app/Http/Controllers/CertificationController.php
+++ app/Http/Controllers/CertificationController.php
@@ -179,9 +179,9 @@ class CertificationController extends Controller
     function getCurrentStatus(Request $request)
     {
         $id = $this->getUser(Cookie::get('token'));
-        if(Cookie::get("admin") == "true" && is_null(Cookie::get("id"))){
+        if(Cookie::get("admin") == "true" && is_null(Cookie::get("current_id"))){
             if(UserCenterController::checkAdmin(UserCenterController::GetUserId(Cookie::get("token")))){
-                return Response::json(array('code' => 200, 'message' => array("您已进入管理员模式","当前修改用户：" + (string)(Cookie::get("id")))));
+                return Response::json(array('code' => 200, 'message' => array("您已进入管理员模式","当前修改用户：" + (string)(Cookie::get("current_id")))));
             } else {
                 abort(403);
             }
