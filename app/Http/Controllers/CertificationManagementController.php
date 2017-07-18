608e33542cb7e483936c87d010e7540c6bbe3584
mmlmml1 <huqingyang2000@gmail.com>
2017-07-18 09:42:11 -0400

    Remove this bug

--- app/Http/Controllers/CertificationManagementController.php
+++ app/Http/Controllers/CertificationManagementController.php
@@ -87,7 +87,7 @@ class CertificationManagementController extends Controller
                 if($this->isInteger($junior,$return))
                     if($this->isInteger($senior_general,$return,6))
                         if($this->isInteger($senior_inter,$return)){
-                            this->$this->sendIdentityMessage("通过",$.cookie("current_id"));
+                            $this->sendIdentityMessage("通过",$.cookie("current_id"));
                             return Response::json(array("code"=>"200"));
                         }
 
