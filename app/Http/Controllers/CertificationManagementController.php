11636d49255b51d3c5195c3eefb1836858cbcea9
mmlmml1 <huqingyang2000@gmail.com>
2017-07-18 00:11:43 -0400

    Update instructions and return value

--- app/Http/Controllers/CertificationManagementController.php
+++ app/Http/Controllers/CertificationManagementController.php
@@ -11,7 +11,17 @@ use Illuminate\Support\Facades\DB;
 class CertificationManagementController extends Controller
 {
     function getInstruction(){
-        $messages = ["注意事项："];
+        if(!UserCenterController::checkAdmin(Cookie::get("token")))
+            abort(403);
+        $messages = "注意事项<br/>".
+        "1. 索引编号小学格式为四位数入学年份，初高中各式位四位数入学年份+两位数班级号（类似于答题卡学号，但无个人学号），中加班级号A-F转换为对应的1-6。<br/>".
+        "2. 索引编号为数组，由半角逗号分隔，每两个索引之间请用一个逗号分割。请不要输入除数字和半角逗号以外的其他内容。<br/>".
+        "3. 默认情况下无需修改服务器自动生成的索引编号。普高编号默认为6个，分别代表从高一到高三6个学期的班级号。其他选项默认都为1个索引。<br/>".
+        "4. 如果存在病假一年等导致班级号变更的情况，请根据说明补充索引。如某南外初中校友是（2000年入学的）06班学生，病假一年后转到了下一届（2001年入学）的07班，则需要将索引修改为'200006,200107'。<br/>".
+        "5. 如果表格存在小问题，可使用编辑功能。只有在大错误的情况下使用拒绝功能，并在消息内注明需要修改的地方。遇到瞎填的用户请直接使用忽略功能。<br/>".
+        "6. 消息栏内的文字将作为消息通知的一部分发送给用户，可根据实际情况填写。<br/>".
+        "7. 审核历史记录可在日志区查询。";
+        return Response::json($messages);
     }
     function getUserDetail(Request $request){
         if(!UserCenterController::checkAdmin(Cookie::get("token")))
@@ -76,9 +86,9 @@ class CertificationManagementController extends Controller
                 if($this->isInteger($junior,$return))
                     if($this->isInteger($senior_general,$return,6))
                         if($this->isInteger($senior_inter,$return))
-                            array_push($return,"检查通过！");
+                            return Response::json(array("code"=>"200"));
         }
-        return Response::json($return);
+        return Response::json(array("code"=>"403","info"=>$return));
     }
 
     function isInteger($data,&$array,$num = -1){
