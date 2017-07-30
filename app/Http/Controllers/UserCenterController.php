9b4ce33ef46e595da99ba0fbb187bc660529257b
mmlmml1 <huqingyang2000@gmail.com>
2017-07-31 01:42:11 +0800

    Add parse

--- app/Http/Controllers/UserCenterController.php
+++ app/Http/Controllers/UserCenterController.php
@@ -144,6 +144,8 @@ class UserCenterController extends Controller
     function getDevice(){
         echo $_SERVER['HTTP_USER_AGENT'];
         $dd = new DeviceDetector($_SERVER['HTTP_USER_AGENT']);
+        $dd->parse();
+
         if ($dd->isBot()) {
             // handle bots,spiders,crawlers,...
             $botInfo = $dd->getBot();
