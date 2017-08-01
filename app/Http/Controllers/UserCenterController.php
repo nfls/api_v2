fe6ce3c3a4a3c1da1209476740cf94745ae52bdf
mmlmml1 <huqingyang2000@gmail.com>
2017-08-01 23:00:01 +0800

    Change url

--- app/Http/Controllers/UserCenterController.php
+++ app/Http/Controllers/UserCenterController.php
@@ -183,13 +183,13 @@ class UserCenterController extends Controller
                 $model = $dd->getModel();
                 if($osInfo['name'] == "iOS"){
                     $allow = false;
-                    $message = "由于iOS的WebKit与本站存在兼容性问题，导致无法正常登陆，请使用我们的<a href='https://api.nfls.io'>客户端</a>进行访问。";
+                    $message = "由于iOS的WebKit与本站存在兼容性问题，导致无法正常登陆，请使用我们的<a href='https://app.nfls.io'>客户端</a>进行访问。";
                 } else {
                     if($clientInfo['name']!="Chrome" && $clientInfo['name']!="Chrome Mobile"){
                         $message = "本站建议使用Chrome进行访问，不保证对于其他浏览器完全兼容。";
                     }
                 }
-                
+
             }
 
         }
