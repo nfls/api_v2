4b19cc217c72a6636d7930b9f9c626703281bced
mmlmml1 <huqingyang2000@gmail.com>
2017-08-24 01:16:31 +0800

    Input

--- app/Http/Controllers/StudentsListController.php
+++ app/Http/Controllers/StudentsListController.php
@@ -13,16 +13,16 @@ class StudentsListController extends Controller
 {
     function getNameList(Request $request){
         if($request->has(["name","session","captcha"] && $request->only(["name","session","captcha"]))){
-            if(UserCenterController::ConfirmCaptcha($request->get("session"), $request->get("captcha"), "nameQuery"))
+            if(UserCenterController::ConfirmCaptcha($request->input("session"), $request->input("captcha"), "nameQuery"))
                 return array("status"=>"failure","message"=>"验证码无效或不正确");
-            $names = DB::connection("mysql_alumni")->table("students")->where(["name"=>$request->get("name")])->get();
+            $names = DB::connection("mysql_alumni")->table("students")->where(["name"=>$request->input("name")])->get();
             foreach($names as $name){
             }
             return Response::json($names);
         } else {
-            echo $request->get("name");
-            echo $request->get("session");
-            echo $request->get("captcha");
+            echo $request->input("name");
+            echo $request->input("session");
+            echo $request->input("captcha");
             abort(404);
         }
     }
