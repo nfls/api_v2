c161c986e7e09f2eb36dfecea93394443f995359
mmlmml1 <huqingyang2000@gmail.com>
2017-08-24 01:25:02 +0800

    no message

--- app/Http/Controllers/StudentsListController.php
+++ app/Http/Controllers/StudentsListController.php
@@ -13,8 +13,8 @@ class StudentsListController extends Controller
 {
     function getNameList(Request $request){
         if($request->has(["name","session","captcha"])){
-            if(UserCenterController::ConfirmCaptcha($request->input("session"), $request->input("captcha"), "nameQuery"))
-                return array("status"=>"failure","message"=>"验证码无效或不正确");
+            //if(!UserCenterController::ConfirmCaptcha($request->input("session"), $request->input("captcha"), "nameQuery"))
+            //    return array("status"=>"failure","message"=>"验证码无效或不正确");
             $names = DB::connection("mysql_alumni")->table("students")->where(["name"=>$request->input("name")])->get();
             foreach($names as $name){
             }
