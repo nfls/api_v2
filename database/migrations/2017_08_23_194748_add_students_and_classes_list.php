c83754bb10e1403bb60e46ef221245dfce0a4e5b
mmlmml1 <huqingyang2000@gmail.com>
2017-08-23 19:59:47 +0800

    Add tables for pre-entered names

---
+++ database/migrations/2017_08_23_194748_add_students_and_classes_list.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStudentsAndClassesList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::connection('mysql_alumni')->create('students', function (Blueprint $table) {
+            $table->increments("id")->index()->comment("ID")->unique();
+            $table->text("name")->index()->nullable()->comment("姓名");
+            $table->boolean("used")->default(false)->comment("是否使用");
+            $table->text("comment")->nullable()->comment("备注");
+        });
+        Schema::connection('mysql_alumni')->create('students', function (Blueprint $table) {
+            $table->increments("id")->index()->unique()->comment("ID")->unique();
+            $table->text("type")->comment("班级类型");
+            $table->integer("year")->comment("届");
+            $table->integer("class")->comment("班");
+            $table->text("comment")->nullable()->comment("备注");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::connection('mysql_alumni')->dropIfExists('students');
+        Schema::connection('mysql_alumni')->dropIfExists('classes');
+    }
+}
