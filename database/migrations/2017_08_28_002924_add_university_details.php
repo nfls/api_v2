cd316357fe873b399ba154475b31ce25da281e63
mmlmml1 <huqingyang2000@gmail.com>
2017-08-28 00:35:52 +0800

    no message

--- database/migrations/2017_08_28_002924_add_university_details.php
+++ database/migrations/2017_08_28_002924_add_university_details.php
@@ -14,7 +14,7 @@ class AddUniversityDetails extends Migration
     public function up()
     {
         //
-        Schema::connection('mysql_alumni')->create('universities', function (Blueprint $table) {
+        Schema::connection('mysql_alumni')->table('universities', function (Blueprint $table) {
             $table->text("shortName")->comment("简写")->nullable();
             $table->text("chineseName")->comment("中文名")->nullable();
             $table->text("chineseShortName")->comment("中文名简写")->nullable();
@@ -29,7 +29,7 @@ class AddUniversityDetails extends Migration
     public function down()
     {
         //
-        Schema::connection('mysql_alumni')->create('universities', function (Blueprint $table) {
+        Schema::connection('mysql_alumni')->table('universities', function (Blueprint $table) {
             $table->dropColumn("shortName");
             $table->dropColumn("chineseName");
             $table->dropColumn("chineseShortName");
