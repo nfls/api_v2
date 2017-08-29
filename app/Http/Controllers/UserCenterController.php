53c8aea6d6d8538370bc95efbcb79d4e616d4c14
mmlmml1 <huqingyang2000@gmail.com>
2017-08-30 03:03:19 +0800

    FInally。。

--- app/Http/Controllers/UserCenterController.php
+++ app/Http/Controllers/UserCenterController.php
@@ -171,7 +171,7 @@ class UserCenterController extends Controller
             $count--;
         $matches = array();
         preg_match('/[A-Za-z0-9_\-\x{0800}-\x{9fa5}]{3,16}/u', $name, $matches);
-        var_dump($matches);
+        //var_dump($matches);
         if ($matches[0] != $name)
             abort(403);
         DB::connection("mysql_forum")->table("nfls_users")->where(["id" => $id])->update(["username" => $name]);
