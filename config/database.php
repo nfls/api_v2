55a2e95ac391198ba75a1cb1dd2d8a2c42be320f
mmlmml1 <huqingyang2000@gmail.com>
2017-09-10 00:44:38 +0800

    Add database for videos

--- config/database.php
+++ config/database.php
@@ -145,11 +145,11 @@ return [
             'engine' => null,
         ],
 
-        'mysql_wiki' => [
+        'mysql_media' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_WIKI', ''),
+            'database' => env('DB_DATABASE_MEDIA', ''),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
--- database/migrations/2017_09_06_165115_add_media_tables.php
+++ database/migrations/2017_09_06_165115_add_media_tables.php
@@ -14,6 +14,19 @@ class AddMediaTables extends Migration
     public function up()
     {
         //
+        Schema::connection('mysql_media')->create("videos", function(Blueprint $table){
+           $table->increments("id")->comment("ID");
+           $table->text("cat")->comment("分区");
+           $table->dateTime("time")->nullable()->comment("上传时间");
+           $table->text("avid")->comment("av号");
+           $table->text("uploader")->comment("UP主");
+           $table->integer("group_id")->default(-1)->comment("组号");
+        });
+        Schema::connection("mysql_alumni")->table("user_auth", function (Blueprint $table){
+            $table->integer("privacy")->nullable()->comment("隐私设置");
+            $table->json("student_info")->nullable()->comment("学生认知信息");
+            $table->boolean("student_auth")->default(false)->comment("学生认证通过");
+        });
     }
 
     /**
@@ -24,5 +37,11 @@ class AddMediaTables extends Migration
     public function down()
     {
         //
+        Schema::connection('mysql_media')->dropIfExists('videos');
+        Schema::connection("mysql_alumni")->table("user_auth", function (Blueprint $table){
+            $table->dropColumn("privacy");
+            $table->dropColumn("student_info");
+            $table->dropColumn("student_auth");
+        });
     }
 }
