8ae26529f6c87fc56d5665ef2c4c648d92418284
mmlmml1 <huqingyang2000@gmail.com>
2017-09-16 19:02:58 +0800

    no message

--- app/Http/Controllers/UserCenterController.php
+++ app/Http/Controllers/UserCenterController.php
@@ -732,8 +732,8 @@ class UserCenterController extends Controller
             $array["bestScore"] = $rank["score"];
             $array["bestRank"] = $rank["count"];
             $array["nowRank"] = $array["bestRank"];
-            $before = DB::connection("mysql_user")->table("user_list")->where("score",">",$rank->score)->orderBy("score","asc")->first();
-            $after = DB::connection("mysql_user")->table("user_list")->where("score","<",$rank->score)->orderBy("score","desc")->first();
+            $before = DB::connection("mysql_user")->table("user_list")->where("score",">",$rank["score"])->orderBy("score","asc")->first();
+            $after = DB::connection("mysql_user")->table("user_list")->where("score","<",$rank["score"])->orderBy("score","desc")->first();
             if(!is_null($before)){
                 $array["playerBefore"]["username"] = self::GetUserNickname($before->id);
                 $array["playerBefore"]["score"] = $before->score;
