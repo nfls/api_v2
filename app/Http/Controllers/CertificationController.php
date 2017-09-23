fa895f2d146d46bd1b80671a00b45aad941b3926
mmlmml1 <huqingyang2000@gmail.com>
2017-09-24 02:38:46 +0800

    。。

--- app/Http/Controllers/CertificationController.php
+++ app/Http/Controllers/CertificationController.php
@@ -1040,9 +1040,7 @@ class CertificationController extends Controller
                 }
             } else {
                 if (@$this->emptyCheck(self::OTHER, $info->phone_international, '手机号码（国外）', $message)) {
-                    if (!preg_match('\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$', (string)($info->phone_domestic))) {
-                        array_push($message, '国外手机号码不正确！');
-                    }
+                    
                 }
             }
         }
