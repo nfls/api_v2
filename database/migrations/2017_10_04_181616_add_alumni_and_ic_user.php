8c5e6eb80ccf13b1fb8a61287ea838cd46ffcbba
mmlmml1 <huqingyang2000@gmail.com>
2017-10-04 18:22:12 +0800

    Add migrations for alumni

---
+++ database/migrations/2017_10_04_181616_add_alumni_and_ic_user.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAlumniAndIcUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::connection("mysql_user")->table("user_list", function (Blueprint $table) {
+            $table->dropColumn("score");
+            $table->dropColumn("lastPlayed");
+            $table->integer("alumni_account")->default(-1);
+            $table->integer("ic_account")->default(-1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::connection("mysql_user")->table("user_list", function (Blueprint $table) {
+            $table->bigInteger("score")->default(0)->comment("游戏分数");
+            $table->time("lastPlayed")->nullable()->comment("上次游戏时间");
+            $table->dropColumn("alumni_account");
+            $table->dropColumn("ic_account");
+        });
+
+    }
+}
