fd36a5692357355a2771e9b471b4df4fa84e3cc5
mmlmml1 <huqingyang2000@gmail.com>
2017-12-09 01:14:58 +0800

    Terrible

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -65,7 +65,7 @@ class WeatherController extends Controller
                 if($configuration["isEnabled"] == true){
                     $flag = false;
                     foreach ($data_asoc as $key=>$value){
-                        if($ins["identification"] == $key){
+                        if($configuration["identification"] == $key){
                             array_push($final_data,$value);
                             $flag = true;
                         }
